baaa104e400808e8008b8963fadbe9a4bafbd712
Gabe Bravo <gabecbravo@gmail.com>
2022-07-09 17:51:42 -0400

    Added a basic recoil atom family for an array of values where each one should have its own atom tracking it

--- src/components/Rectangle/Rectangle.tsx
+++ src/components/Rectangle/Rectangle.tsx
@@ -1,5 +1,5 @@
-import {useContext} from 'react'
-import {ElementsContext, SelectedElementContext} from '../../Canvas'
+import {useRecoilState, atomFamily} from 'recoil'
+import {selectedElementState} from '../../Canvas'
 import {Drag} from '../Drag'
 import {RectangleContainer} from './RectangleContainer'
 import {RectangleInner} from './RectangleInner'
@@ -11,15 +11,37 @@ export type ElementStyle = {
 
 export type Element = {style: ElementStyle}
 
-export const Rectangle = ({element, index}: {element: Element; index: number}) => {
-    const {selectedElement, setSelectedElement} = useContext(SelectedElementContext)
-    const {setElement} = useContext(ElementsContext)
+/*  https://recoiljs.org/docs/api-reference/utils/atomFamily
+    An Atom Family represents a collection of atoms. 
+    When you call atomFamily() it will return a function which provides the RecoilState atom 
+    based on the parameters you pass in.
+*/
+
+// atomFamily = Returns a function that returns a writeable RecoilState atom.
+// used for creating multiple atoms that are the same atom type
+// when called on line 29, the dynamic id becomes like a compound key
+const elmentState = atomFamily<Element, number>({
+    key: 'element',
+    default: {
+        style: {
+            position: {top: 0, left: 0},
+            size: {width: 50, height: 50},
+        },
+    },
+})
+
+export const Rectangle = ({id}: {id: number}) => {
+    const [selectedElement, setSelectedElement] = useRecoilState(selectedElementState)
+    const [element, setElement] = useRecoilState(elmentState(id)) // the id is the parameter here
+    // The atomFamily() essentially provides a map from the parameter to an atom.
+    // You only need to provide a single key for the atom family and it will generate a unique key for each underlying atom.
+    // These atom keys can be used for persistence, and so must be stable across application executions.
 
     return (
         <Drag
             position={element.style.position}
             onDrag={(position) => {
-                setElement(index, {
+                setElement({
                     style: {
                         ...element.style,
                         position,
@@ -32,10 +54,10 @@ export const Rectangle = ({element, index}: {element: Element; index: number}) =
                     position={element.style.position}
                     size={element.style.size}
                     onSelect={() => {
-                        setSelectedElement(index)
+                        setSelectedElement(id)
                     }}
                 >
-                    <RectangleInner selected={index === selectedElement} />
+                    <RectangleInner selected={id === selectedElement} />
                 </RectangleContainer>
             </div>
         </Drag>
